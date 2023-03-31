7fef023beb568c8709965efc99aa6b4cceff3a6e
Stewie Nguyen <mopro1010@gmail.com>
2023-03-31 08:57:15 +0700

    First Commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace WordFlex_Program;
+class Program
+{
+    static void Main(string[] args)
+    {
+        WordFlex();
+        Console.ReadKey();
+    }
+
+    public static void WordFlex()
+    {
+        // initializing the arrays
+        string[] wordsFlexA = new string[] {"Sponge", "Giggle", "Beetroot", "Watermelon"};
+        string[] wordsFlexB = {"Sus", "Amogus", "Video games", "FPS"};
+
+        Console.WriteLine("Select an exam question: A/B");
+        string input = Console.ReadLine()!;
+        if (input == "A" || input == "a")
+        {
+            for (int i = 0; i < wordsFlexA.Length; i++)
+            {
+                Random random = new Random(); // getting a random num
+                int randomWord = random.Next(0, wordsFlexA.Length);
+                Console.WriteLine(wordsFlexA[randomWord]); // Write the word corresponding to the index
+                Console.WriteLine("Press enter to add a new word, press anything else to stop");
+                string enter = Console.ReadLine()!;
+                if (enter == "")
+                {
+                    continue;
+                } else {break;}
+            }
+        }
+        if (input == "B" || input == "b")
+        {
+            for (int i = 0; i < wordsFlexB.Length; i++)
+            {
+                Random random = new Random();
+                int randomWord = random.Next(0, wordsFlexB.Length);
+                Console.WriteLine(wordsFlexB[randomWord]);
+                Console.WriteLine("Press enter to add a new word, press anything else to stop");
+                string enter = Console.ReadLine()!;
+                if (enter == "")
+                {
+                    continue;
+                } else {break;}
+            }
+            
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+#WordFlex Program
+Recently learned arrays and I wanted to try it out so here's a simple program that generate 4 words as you press enter. 
+
+#Things I might add in the future (if I'm not lazy)
+-> True Random Words (Currently it repeats words that were already given)
+
+#Note
+This is more like a library of small projects that I do to get used to Git and Github. Alongside that, I can save my projects in many repository and I can come back to check it out in the future. In general, it's my study diary.
